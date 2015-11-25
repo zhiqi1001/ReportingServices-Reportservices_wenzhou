b19e363cb78897da50828b430734201526332bac
zhiqi1001 <zhiqi1001@qq.com>
2015-11-25 10:49:54 +0800

    addAll
    新增项目

---
+++ ReportingServices/WSConfirm.asmx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SqlClient;
+using Trirand.Web.UI.WebControls;
+using System.Web.Script.Services;
+using System.Configuration;
+
+using Envir_ExceptionGroup;
+
+namespace ReportingServices
+{
+    /// <summary>
+    /// WebService1 的摘要说明
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    [System.Web.Script.Services.ScriptService] 
+    public class WSConfirm : System.Web.Services.WebService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="date">所要提交的日数据日期</param>
+        [WebMethod(Description = "提交日数据")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public opermsg submit(string datestr)
+        {
+            try
+            {
+                AppSettingsReader asr = new AppSettingsReader();
+                string dbconn = (string)asr.GetValue("dbconn", typeof(string));
+                using (SqlConnection conn = new SqlConnection(dbconn))
+                {
+                    conn.Open();
+                    SqlCommand sqlcomm = new SqlCommand("insert into Exception_DayConfirm(timestamps,confirmed) values('" + datestr + "',1)", conn);
+                    sqlcomm.ExecuteNonQuery();
+                    conn.Close();
+                }
+                return new opermsg { status = 1, msg = "该日数据已提交" };
+            }
+            catch (Exception ex)
+            {
+                return new opermsg { status = 0, msg = ex.Message };
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="datestr"></param>
+        /// <returns></returns>
+        [WebMethod(Description = "查看提交状态")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public int checkdaysubmitstatus(string datestr)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                int count;
+                AppSettingsReader asr = new AppSettingsReader();
+                string dbconn = (string)asr.GetValue("dbconn", typeof(string));
+                using (SqlConnection conn = new SqlConnection(dbconn))
+                {
+                    conn.Open();
+                    SqlCommand sqlcomm = new SqlCommand("select count(*) from Exception_DayConfirm where timestamps='" + datestr + "'", conn);
+                    count = (int)sqlcomm.ExecuteScalar();
+                    conn.Close();
+                }
+                return count;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="st"></param>
+        /// <param name="et"></param>
+        /// <returns></returns>
+        [WebMethod(Description = "小时均值分组操作")]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public int houravggroup(DateTime st, DateTime et)
+        {
+            return (new Envir_ExceptionGroup.Common()).houravggroup(st, et);
+        }
+
+    }
+}
---
+++ ReportingServices/obj/Debug/AspnetCompileMerge/TempBuildDir/Reports.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Reports.aspx.cs" Inherits="ReportingServices.Reports" MasterPageFile="~/ReportingServices.Master" %>
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=11.0.0.0, Culture=neutral, PublicKeyToken=89845DCD8080CC91"
+    Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<asp:Content ID="content1" ContentPlaceHolderID="pagecontent" runat="server">
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server" Font-Names="Verdana" Font-Size="8pt" Height="1209px" WaitMessageFont-Names="Verdana" WaitMessageFont-Size="14pt" Width="1200px" ProcessingMode="Remote">
+            <ServerReport ReportPath="/ReportList"/>
+        </rsweb:ReportViewer>
+    </div>
+    </form>
+</asp:Content>
